f5ebff74cd47ea2e38eeb8d1ae234f1445b757a1
Jason Hazel <jhazel@matrixmediaservices.com>
2021-11-28 14:08:36 -0500

    change to trigger online message after other sockets initialized

--- sockets/general/index.js
+++ sockets/general/index.js
@@ -2,7 +2,8 @@ const { sockets } = require('..')
 
 module.exports = (ws) => {
   ws._send = general => ws.$send({ general })
+  sockets(__dirname).map(file => require(file)(ws))
+  
   ws._send({ online: true })
 
-  sockets(__dirname).map(file => require(file)(ws))
 }
